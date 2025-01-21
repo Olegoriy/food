8594e11256db6fa560a4dbd808fd7887f9001acc
Olegoriy <olegkib1506@gmail.com>
2025-01-21 18:21:58 +0300

    add: modal window

--- js/script.js
+++ js/script.js
@@ -99,4 +99,25 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadLine);
+
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn =>{
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    });
 });
