b86eb13d185bbddb40e491daa5d14cbbe57070d2
Olegoriy <olegkib1506@gmail.com>
2025-02-04 14:40:26 +0300

    add: menu cards with json-server

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,19 +1,2 @@
-const urlObj = {
-    protocol: 'https',
-    domain: 'mysite.com'
-}
-
-function showCurrentURL() {
-    const extractCurrDomain = () => {
-        return this.domain;
-    }
-    const extractCurrProtocol = () => {
-        return this.protocol;
-    }
-
-    console.log(`${extractCurrProtocol()}://${extractCurrDomain()}`)
-}
-
-const url = showCurrentURL.bind(urlObj);
-
-console.log(url);
+const obj = {a: 23, b: 50};
+        console.log(Object.entries(obj));
