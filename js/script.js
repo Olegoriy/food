4e3e0444264d29206bf3f5ca3b8f1529c37c1db2
Olegoriy <olegkib1506@gmail.com>
2025-02-11 21:18:03 +0300

    add: function for calculate calories

--- js/script.js
+++ js/script.js
@@ -486,5 +486,23 @@ fetch('http://localhost:3000/menu')
             dots[slideIndex - 1].style.opacity = 1;
         });
     });
+    // Calc
 
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+        } else {
+            result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+        }
+    }
+
+    calcTotal();
 });
